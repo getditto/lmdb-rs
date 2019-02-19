0b1e04649777f7239d2da8649b8b3f375bf42463
Hamilton Chapman <hamchapman@gmail.com>
2019-02-19 17:38:45 +0000

    Update lmdb to 0.9.23 and use cc in liblmdb-sys build.rs

--- liblmdb-sys/build.rs
+++ liblmdb-sys/build.rs
@@ -1,16 +1,28 @@
-extern crate gcc;
+extern crate cc;
+
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let target = std::env::var("TARGET").unwrap();
 
-    let mut config = gcc::Config::new();
-    config.file("mdb/libraries/liblmdb/mdb.c")
-          .file("mdb/libraries/liblmdb/midl.c");
+    let mut lmdb: PathBuf = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
+    lmdb.push("mdb");
+    lmdb.push("libraries");
+    lmdb.push("liblmdb");
+
+    let mut config = cc::Build::new();
+    config.file(lmdb.join("mdb.c"))
+          .file(lmdb.join("midl.c"));
     config.opt_level(2);
 
     if target.contains("dragonfly") {
         config.flag("-DMDB_DSYNC=O_SYNC");
         config.flag("-DMDB_FDATASYNC=fsync");
+    }
+
+    if target.contains("android") {
+        config.define("ANDROID", "1");
     }
 
     config.compile("liblmdb.a");
