a005bfc2fdb98fd23fc4d0f067faa9ce69e33294
Yurii Rashkovskii <yrashk@gmail.com>
2019-10-17 17:07:12 -0700

    Problem: LMDB operations fail with Code 22 error
    Often times, when running LMDB operations in a multithreaded environment on
    macOS, especially a heavily multithreaded one, various operations sporadically
    fail with Code 22
    
    Solution: use POSIX threads
    
    Turns out, this issue is present only when using semaphores, but not POSIX
    mutexes. Not exactly sure why but switching over solves the problem.

--- liblmdb-sys/build.rs
+++ liblmdb-sys/build.rs
@@ -23,10 +23,9 @@ fn main() {
 
     if target.contains("android") {
         config.define("ANDROID", "1");
-    }
-
-    if target.contains("ios") {
-        config.define("MDB_USE_POSIX_SEM", "1");
+    } else if !target.contains("linux") {
+	config.define("MDB_USE_POSIX_MUTEX", "1");
+	config.define("MDB_USE_ROBUST", "0");
     }
 
     config.compile("liblmdb.a");
